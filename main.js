7e8f0f47528b0d534a003b38151a7bb47e2e7a26
Isming <email@example.com>
2026-03-14 17:17:24 +0500

    java siciript fayilini ochdim

---
+++ main.js
@@ -0,0 +1,65 @@
+// let arr =[1,2,3,4,5,];
+// arr.push(5)
+// console.log(arr);
+
+
+// let  arr =  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+// let arr1 = arr.filter(el => el % 2 !== 0);
+
+// console.log(arr1
+
+
+
+// let arr =["ali", "vali", "hasan", "husan",]
+// let NewArr = arr.filter()
+
+// console.log(arr);
+// buni ham istozdan soriman
+
+// let arr =["ali", "vali", "hasan", "husan",]
+// let NewArr = arr.push("farhod")
+
+// console.log(arr);
+// boshidan qoshib beradi
+
+
+// let arr =["ali", "vali", "hasan", "husan",]
+// let NewArr = arr.unshift("farhod" )
+
+// console.log(arr);
+
+// boshiga qoshdi
+
+
+// let arr =["ali", "vali", "hasan", "husan",]
+// let NewArr = arr.concat("farhod")
+
+// console.log(arr);
+// nima qilinishini ustozdan soriman
+
+
+// let arr =["ali", "vali", "hasan", "husan",]
+// let NewArr = arr.pop()
+
+// console.log(arr);
+// // ohiridan ochirdi
+
+
+
+
+// let arr =["ali", "vali", "hasan", "husan",]
+// let NewArr = arr.shift()
+
+// console.log(arr);
+// boshiidan ochirvooti
+
+
+
+
+// let arr =["ali", "vali", "hasan", "husan",]
+// let NewArr = arr.splice()
+
+// console.log(arr);
+// 1-elementni qaytarib bervooti
+    
